4534411626c95ae231af31b9477a5dd9dbbfa940
naol16 <lemesanaol93@gmail.com>
2025-01-14 23:57:06 +0300

    new changes

--- frontend/realtimechatapp/src/component/userandgrouplist/userandgrouplist.jsx
+++ frontend/realtimechatapp/src/component/userandgrouplist/userandgrouplist.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import  axiosurl from "../../../axios/axios"
 
 function Usergrouplist() {
   return (
     <div className='flex  bg-radial-gradient min-h-screen'>
-      <div  className="w-[33%] bg-[#111111]">
-       <div className='pt-5 pl-3'><SearchIcon className="text-white"/>
+      <div  className="w-[33%] bg-[#111111] p-5">
+       <div className='flex items-center space-x-3'><SearchIcon className="text-white"/>
           <input
           type="text"
           // value={searchitem}
           // onChange={handleinput}
           placeholder="type the title of dsa"
-          className="text-black w-[20vw] rounded-md "
+          className="text-black  w-full sm:w-[20vw] rounded-md  py-2 px-3"
           >
-          </input>
-          </div>
-        <div className='flex flex-col justify-center'>
-              <p><PersonRoundedIcon className='bg-white w-[32] rounded-lg ml-12 mt-5 mr-4' /> 
-              <span className='text-white my-auto mx-auto'> naol</span></p>
-      
+          </input>      
+           </div>
+        <div className='flex items-center mt-6'>
+              <p className='flex ml-12 mt-4'><PersonRoundedIcon className='bg-white w-[32] rounded-lg   mr-4' /> 
+              <span className='text-white '> naol</span></p>
         </div>
           </div>
       
